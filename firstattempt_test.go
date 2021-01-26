9f5ea34ac79947b1920e79979e83ce855bdae413
Zachary Burk <zachburk7@gmail.com>
2021-01-25 22:07:33 -0500

    Add tests

--- firstattempt_test.go
+++ firstattempt_test.go
@@ -2,6 +2,15 @@ package main
 
 import "testing"
 
+func TestUnoptimized(t *testing.T) {
+	wordsInPhrase := unoptimized()
+
+	numberOfWordsReturned := len(wordsInPhrase)
+	if numberOfWordsReturned != 3 {
+		t.Errorf("Returned %d words; want 3", numberOfWordsReturned)
+	}
+}
+
 func BenchmarkPhraseGenerationFirstAttempt(b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		unoptimized()
--- optimized_test.go
+++ optimized_test.go
@@ -2,6 +2,15 @@ package main
 
 import "testing"
 
+func TestOptimized(t *testing.T) {
+	wordsInPhrase := optimized()
+
+	numberOfWordsReturned := len(wordsInPhrase)
+	if numberOfWordsReturned != 3 {
+		t.Errorf("Returned %d words; want 3", numberOfWordsReturned)
+	}
+}
+
 func BenchmarkPhraseGenerationOptimized(b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		optimized()
